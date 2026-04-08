2413c778987da0bab4f359df03720ecc67362166
Md Hamid Azam <hm2563749@gmail.com>
2026-04-08 08:18:58 +0530

    Added Pair In Array

---
+++ Arrays/PairInArray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class PairInArray {
+    public static void Pairs(int[] arr) {
+        int totalPair = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + curr + "," + arr[j] + ")");
+                totalPair++;
+            }
+            System.out.println();
+        }
+        System.out.println("The total pair of this Array is: " + totalPair);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 6, 8, 10 };
+        Pairs(arr);
+    }
+}
