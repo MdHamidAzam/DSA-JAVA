ff7463eb0fac2fb85566c3cc37a63f2842d1e994
Md Hamid Azam <hm2563749@gmail.com>
2026-06-17 23:49:39 +0530

    Added Best Time to Buy And Sell Stock

---
+++ Arrays/BuyAndSellStock.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class BuyAndSellStock {
+    public static void BuyAndSellStock(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int mp = Integer.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+
+            if(arr[i] - min > mp) {
+                mp = arr[i] - min;
+            }
+        }
+        System.out.println(mp);
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 1, 5, 3, 6, 4};
+        BuyAndSellStock(arr);
+        
+    }
+}
