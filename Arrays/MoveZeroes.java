ac681235a5235954304d09acdcd725c8df677688
Md Hamid Azam <hm2563749@gmail.com>
2026-07-24 17:55:46 +0530

    Updating code

--- Arrays/MoveZeroes.java
+++ Arrays/MoveZeroes.java
@@ -2,32 +2,33 @@ package Arrays;
 
 public class MoveZeroes {
     public static void MoveZeroes(int[] arr) {
-        int NoOfZero = 0;
-        int n = arr.length;
+        // int NoOfZero = 0;
+        // int n = arr.length;
 
-        for(int ele : arr) {
-            if(ele == 0) NoOfZero++;
-        }
+        // for(int ele : arr) {
+        //     if(ele == 0) NoOfZero++;
+        // }
 
-        for(int x=0; x<NoOfZero; x++) {
-            for(int i=0; i<n-x-1; i++) {
-                if(arr[i] == 0) {
-                    int temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
-                }
-            }
-        }
-
-        // Two pointer Approah
-        // for(int i = 0; i < arr.length; i++) {
-        //     if(arr[i] != 0) {
-        //         int temp = arr[i];
-        //         arr[i] = arr[j];
-        //         arr[j] = temp;
-        //         j++;
+        // for(int x=0; x<NoOfZero; x++) {
+        //     for(int i=0; i<n-x-1; i++) {
+        //         if(arr[i] == 0) {
+        //             int temp = arr[i];
+        //             arr[i] = arr[i+1];
+        //             arr[i+1] = temp;
+        //         }
         //     }
         // }
+
+        // Two pointer Approah
+        int j=0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
     }
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 3, 12};
