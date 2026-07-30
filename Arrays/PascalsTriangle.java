5f0fc72b3a67cd6f120f7965b0f150ec0514f199
Md Hamid Azam <hm2563749@gmail.com>
2026-07-30 17:32:28 +0530

    Updating PascalTraingle

--- Arrays/PascalsTriangle.java
+++ Arrays/PascalsTriangle.java
@@ -2,25 +2,25 @@ package Arrays;
 
 import java.util.*;
 
-public class PascalTriangle {
+public class PascalsTriangle {
 
     public static List<List<Integer>> generate(int n) {
+
         List<List<Integer>> ans = new ArrayList<>();
 
-        // Create rows
         for (int i = 0; i < n; i++) {
             List<Integer> row = new ArrayList<>();
+
             for (int j = 0; j <= i; j++) {
                 row.add(1);
             }
+
             ans.add(row);
         }
 
-        // Fill middle elements
         for (int i = 2; i < n; i++) {
             for (int j = 1; j <= i - 1; j++) {
-                ans.get(i).set(j,
-                        ans.get(i - 1).get(j) + ans.get(i - 1).get(j - 1));
+                ans.get(i).set(j, ans.get(i - 1).get(j) + ans.get(i - 1).get(j - 1));
             }
         }
 
@@ -28,18 +28,13 @@ public class PascalTriangle {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+        int n = 5;
 
         List<List<Integer>> result = generate(n);
 
-        System.out.println("\nPascal's Triangle:");
         for (List<Integer> row : result) {
             System.out.println(row);
         }
-
-        sc.close();
     }
 }
