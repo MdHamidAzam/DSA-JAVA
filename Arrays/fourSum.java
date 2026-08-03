9809db8a556c735b07899212de50de03a1626fa6
Md Hamid Azam <hm2563749@gmail.com>
2026-08-03 18:12:13 +0530

    Adding fourSum

---
+++ Arrays/fourSum.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class fourSum {
+    public static void fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        System.out.println("Quadruplet: " + nums[i] + ", " + nums[j] + ", " + nums[left] + ", " + nums[right]);
+                        while (left < right && nums[left] == nums[left + 1]) left++; // Skip duplicates
+                        while (left < right && nums[right] == nums[right - 1]) right--; // Skip duplicates
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0);
+    }
+}
