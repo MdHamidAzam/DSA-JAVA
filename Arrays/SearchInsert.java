3a27ae80d1f6a170a6f3b763d22005d40ff9c4fe
Md Hamid Azam <hm2563749@gmail.com>
2026-08-08 21:56:08 +0530

    Udating SearchInsert

--- Arrays/SearchInsert.java
+++ Arrays/SearchInsert.java
@@ -4,24 +4,25 @@ public class SearchInsert {
     public static void SearchInsert(int[] arr) {
         int target = 3;
         int low = 0;
-        int high = arr.length-1;
+        int high = arr.length - 1;
 
-        while(low<=high) {
-            int mid = low + (high-low)/2;
-            if(arr[mid] == target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
                 System.out.println(mid);
                 return;
-            } else if(arr[mid]>target) {
-                high = mid-1;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
             } else {
-                low = mid+1;
+                low = mid + 1;
             }
         }
 
         System.out.println(low);
     }
+
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 6};
+        int[] arr = { 1, 3, 5, 6 };
         SearchInsert(arr);
     }
 }
