01ed48e5f13230f7845be527a09ac85cf88e58ae
Md Hamid Azam <hm2563749@gmail.com>
2026-08-08 21:57:03 +0530

    Updating SingleNumber

--- Arrays/SingleNumber.java
+++ Arrays/SingleNumber.java
@@ -3,13 +3,14 @@ package Arrays;
 public class SingleNumber {
     public static void SingleNumber(int[] arr) {
         int value = 0;
-        for(int i=0; i<arr.length; i++) {
-            value = value^arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            value = value ^ arr[i];
         }
         System.out.println(value);
     }
+
     public static void main(String[] args) {
-        int arr[] = {4, 1, 2, 1, 2};
+        int arr[] = { 4, 1, 2, 1, 2 };
         SingleNumber(arr);
     }
 }
