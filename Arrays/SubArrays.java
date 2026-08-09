f6e5d1c22a42a3589d38d11834b96e0ad93be82a
Md Hamid Azam <hm2563749@gmail.com>
2026-08-09 21:37:37 +0530

    Updating SubArrays

--- Arrays/SubArrays.java
+++ Arrays/SubArrays.java
@@ -3,23 +3,23 @@ package Arrays;
 public class SubArrays {
     public static void printsubaarays(int[] arr) {
         int totalsubarray = 0;
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             int start = i;
-            for(int j=i; j<arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 int end = j;
-                for(int k=start; k<=end; k++) {
-                    System.out.print(arr[k]+" ");
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ");
                 }
                 totalsubarray++;
                 System.out.println();
             }
             System.out.println();
         }
-        System.out.println("The total subarray of this Array is: "+totalsubarray);
+        System.out.println("The total subarray of this Array is: " + totalsubarray);
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 4, 6, 8, 10};
+        int[] arr = { 2, 4, 6, 8, 10 };
         printsubaarays(arr);
     }
 }
