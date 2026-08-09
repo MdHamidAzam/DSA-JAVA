8716baf253defe45ffc22b7075ca6f3dae6a1a28
Md Hamid Azam <hm2563749@gmail.com>
2026-08-09 21:38:32 +0530

    Updating SpiralMatrix

--- Arrays/SpiralMatrix.java
+++ Arrays/SpiralMatrix.java
@@ -4,38 +4,44 @@ public class SpiralMatrix {
     public static void SpiralMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int minr = 0; int maxr = m - 1;
-        int minc = 0; int maxc = n - 1;
+        int minr = 0;
+        int maxr = m - 1;
+        int minc = 0;
+        int maxc = n - 1;
 
-        while(minr<=maxr && minc<=maxc) {
+        while (minr <= maxr && minc <= maxc) {
             // left to right
-            for(int i=minc; i<=maxc; i++) {
+            for (int i = minc; i <= maxc; i++) {
                 System.out.print(matrix[minr][i] + " ");
             }
             minr++;
 
             // top to bottom
-            if(minr>maxr || minc>maxc) break;
-            for(int i=minr; i<=maxr; i++) {
+            if (minr > maxr || minc > maxc)
+                break;
+            for (int i = minr; i <= maxr; i++) {
                 System.out.print(matrix[i][maxc] + " ");
             }
             maxc--;
 
             // right to left
-            if(minr>maxr || minc>maxc) break;
-            for(int i=maxc; i>=minc; i--) {
+            if (minr > maxr || minc > maxc)
+                break;
+            for (int i = maxc; i >= minc; i--) {
                 System.out.print(matrix[maxr][i] + " ");
             }
             maxr--;
 
             // bottom to top
-            if(minr>maxr || minc>maxc) break;
-            for(int i=maxr; i>=minr; i--) {
+            if (minr > maxr || minc > maxc)
+                break;
+            for (int i = maxr; i >= minr; i--) {
                 System.out.print(matrix[i][minc] + " ");
             }
             minc++;
         }
     }
+
     public static void main(String[] args) {
         int matrix[][] = {
                 { 1, 2, 3 },
