ee23aa43359089d7b9781a66d1471e3988e59081
Md Hamid Azam <hm2563749@gmail.com>
2026-08-09 21:36:24 +0530

    Updating WiggleSortII

--- Arrays/WiggleSortII.java
+++ Arrays/WiggleSortII.java
@@ -7,21 +7,22 @@ public class WiggleSortII {
         Arrays.sort(nums);
         int n = nums.length;
         int[] temp = new int[n];
-        int left = (n-1)/2;
-        int high = n-1;
+        int left = (n - 1) / 2;
+        int high = n - 1;
 
-        for(int i=0; i<n; i++) {
-            if(i%2 == 0) {
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
                 temp[i] = nums[left--];
             } else {
                 temp[i] = nums[high--];
             }
         }
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             nums[i] = temp[i];
         }
     }
+
     public static void main(String[] args) {
         int[] nums = { 1, 5, 1, 1, 6, 4 };
         wiggleSort(nums);
