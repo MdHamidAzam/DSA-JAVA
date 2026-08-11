561a2484affd60457d20bd981bb2c84cf6e31e8e
Md Hamid Azam <hm2563749@gmail.com>
2026-08-11 22:16:05 +0530

    Updating ContainsDuplicate

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -2,9 +2,9 @@ package Arrays;
 
 public class ContainsDuplicate {
     public static void ContainsDuplicate(int[] arr) {
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i+1; j<arr.length; j++) {
-                if(arr[i] == arr[j]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
                     System.out.println(true);
                     return;
                 }
@@ -12,8 +12,9 @@ public class ContainsDuplicate {
         }
         System.out.println(false);
     }
+
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4};
+        int arr[] = { 1, 2, 3, 4 };
         ContainsDuplicate(arr);
     }
 }
