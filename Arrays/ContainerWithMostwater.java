3851f07be5acf98dcc308db8d883358580c70040
Md Hamid Azam <hm2563749@gmail.com>
2026-08-11 22:11:47 +0530

    Updating ContainerWithMostwater

--- Arrays/ContainerWithMostwater.java
+++ Arrays/ContainerWithMostwater.java
@@ -5,15 +5,15 @@ public class ContainerWithMostwater {
         int n = height.length;
         int maxWater = 0;
         int lp = 0; // left pointer
-        int rp = n-1; // right poointer
+        int rp = n - 1; // right poointer
 
-        while(lp < rp) {
+        while (lp < rp) {
             int width = rp - lp;
             int ht = Math.min(height[lp], height[rp]);
             int currWater = width * ht; // area
             maxWater = Math.max(currWater, maxWater);
 
-            if(height[lp] < height[rp]) {
+            if (height[lp] < height[rp]) {
                 lp++;
             } else {
                 rp--;
@@ -22,8 +22,9 @@ public class ContainerWithMostwater {
 
         System.out.println(maxWater);
     }
+
     public static void main(String[] args) {
-        int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
         ContainerWithMostwater(height);
     }
 }
