25621da3d9000085dd81ebe1dc76a37e7c17afd8
Md Hamid Azam <hm2563749@gmail.com>
2026-08-19 16:22:43 +0530

    Updating Greatest Common Divisor of Strings

--- Strings/GCDofString.java
+++ Strings/GCDofString.java
@@ -2,18 +2,40 @@ package Strings;
 
 public class GCDofString {
     public static String GCD(String str1, String str2) {
+    //     int m = str1.length();
+    //     int n = str2.length();
+
+    //     if(!(str1 + str2).equals(str2 + str1)) return "";
+
+    //     while(n != 0) {
+    //         int temp = n;
+    //         n = m%n;
+    //         m = temp;
+    //     }
+
+    //     return str1.substring(0, m);
+    // }
+
+    if (!(str1 + str2).equals(str2 + str1)) return "";
+
         int m = str1.length();
         int n = str2.length();
 
-        if(!(str1 + str2).equals(str2 + str1)) return "";
-
-        while(n != 0) {
-            int temp = n;
-            n = m%n;
-            m = temp;
+        if (m < n) {
+            int temp = m;
+            m = n;
+            n = temp;
         }
 
-        return str1.substring(0, m);
+        int gcd = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (m % i == 0 && n % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return str1.substring(0, gcd);
     }
     public static void main(String[] args) {
         String str1 = "ABCABC";
