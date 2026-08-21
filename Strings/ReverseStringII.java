7e27775cb300f9b7ba2b567748e8a6aa7c5beb58
Md Hamid Azam <hm2563749@gmail.com>
2026-08-21 16:02:56 +0530

    Adding ReverseStringII

---
+++ Strings/ReverseStringII.java
@@ -0,0 +1,27 @@
+package Strings;
+
+public class ReverseStringII {
+    public static String reverseString(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        for(int i=0; i<n; i += 2*k) {
+            int left = i;
+            int right = Math.min(i+k-1, n-1);
+
+            while(left < right) {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(arr);
+    }
+    public static void main(String[] args) {
+        String s = "abcd";
+        System.out.println(reverseString(s, 2));
+    }
+}
