a771f9c5334bb8eceff3ca33e02f4053df06b603
Md Hamid Azam <hm2563749@gmail.com>
2026-08-22 15:53:28 +0530

    Adding GrooupAnagrams

---
+++ Strings/GroupAnagrams.java
@@ -0,0 +1,27 @@
+package Strings;
+
+import java.util.*;
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs) {
+            char[] ch = str.toCharArray();
+
+            Arrays.sort(ch);
+
+            String key = new String(ch);
+
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
