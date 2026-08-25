e382e33d9a00d5e31c7979854f7503ec896b689b
Md Hamid Azam <hm2563749@gmail.com>
2026-08-25 19:45:38 +0530

    Updating FindMininRotatedSortedArray

--- Arrays/FindMininRotatedSortedArray.java
+++ Arrays/FindMininRotatedSortedArray.java
@@ -4,16 +4,19 @@ public class FindMininRotatedSortedArray {
     public static int findMin(int[] nums) {
         int n = nums.length;
         int low = 0;
-        int high = n-1;
+        int high = n - 1;
 
-        while(low < high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] >  nums[high]) low = mid + 1;
-            else high = mid;
+            if (nums[mid] > nums[high])
+                low = mid + 1;
+            else
+                high = mid;
         }
 
         return nums[low];
-    } 
+    }
+
     public static void main(String[] args) {
         int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
         System.out.println(findMin(nums));
