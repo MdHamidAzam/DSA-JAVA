62153c3252d9ca8b44b1bce843acafa09d96ca8e
Md Hamid Azam <hm2563749@gmail.com>
2026-08-26 20:10:36 +0530

    Updating ReorganizeString

--- Strings/ReorganizeString.java
+++ Strings/ReorganizeString.java
@@ -5,33 +5,35 @@ public class ReorganizeString {
         int n = s.length();
         int[] freq = new int[26];
 
-        for(char ch : s.toCharArray()) {
+        for (char ch : s.toCharArray()) {
             freq[ch - 'a']++;
         }
 
         int maxFreq = 0;
         int maxChar = 0;
 
-        for(int i=0; i<26; i++) {
-            if(freq[i] > maxFreq) {
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > maxFreq) {
                 maxFreq = freq[i];
                 maxChar = i;
             }
         }
 
-        if(maxFreq > (n+1)/2) return "";
+        if (maxFreq > (n + 1) / 2)
+            return "";
 
         char[] result = new char[n];
         int index = 0;
-        while(freq[maxChar] > 0) {
+        while (freq[maxChar] > 0) {
             result[index] = (char) ('a' + maxChar);
             freq[maxChar]--;
             index += 2;
         }
 
-        for(int i=0; i<26; i++) {
-            while(freq[i] > 0) {
-                if(index >= n) index = 1;
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] > 0) {
+                if (index >= n)
+                    index = 1;
                 result[index] = (char) ('a' + i);
                 freq[i]--;
                 index += 2;
@@ -40,6 +42,7 @@ public class ReorganizeString {
 
         return new String(result);
     }
+
     public static void main(String[] args) {
         String s = "aab";
         System.out.println(reorganizeString(s));
