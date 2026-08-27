fbe78392d562380688340bcd17c591eeb0244925
Md Hamid Azam <hm2563749@gmail.com>
2026-08-27 21:27:21 +0530

    Updating NumberofSubstringsWithOnly1s

--- Strings/NumberofSubstringsWithOnly1s.java
+++ Strings/NumberofSubstringsWithOnly1s.java
@@ -6,15 +6,17 @@ public class NumberofSubstringsWithOnly1s {
         int count = 0;
         long answer = 0;
 
-        for(int i=0; i<n; i++) {
-            if(s.charAt(i) == '1') {
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
                 count++;
-                answer = (answer +count) % 1000000007;
-            } else count = 0;
+                answer = (answer + count) % 1000000007;
+            } else
+                count = 0;
         }
 
         return (int) answer;
     }
+
     public static void main(String[] args) {
         String s = "0110111";
         System.out.println(numSub(s));
