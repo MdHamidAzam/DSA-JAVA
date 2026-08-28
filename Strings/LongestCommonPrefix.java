f9fbdb275b8eb1898320d1b92262e8e3e2358d98
Md Hamid Azam <hm2563749@gmail.com>
2026-08-28 22:13:50 +0530

    Updating LongestCommonPrefix

--- Strings/LongestCommonPrefix.java
+++ Strings/LongestCommonPrefix.java
@@ -6,17 +6,20 @@ public class LongestCommonPrefix {
         int m = strs.length;
         int n = first.length();
 
-        for(int i=0; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                if(i >= strs[j].length()) return first.substring(0,i);
-                if(first.charAt(i) != strs[j].charAt(i)) return first.substring(0,i);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (i >= strs[j].length())
+                    return first.substring(0, i);
+                if (first.charAt(i) != strs[j].charAt(i))
+                    return first.substring(0, i);
             }
         }
 
         return first;
     }
+
     public static void main(String[] args) {
-        String[] strs = {"flower","flow","flight"};
+        String[] strs = { "flower", "flow", "flight" };
         System.out.println(longestCommonPrefix(strs));
     }
 }
