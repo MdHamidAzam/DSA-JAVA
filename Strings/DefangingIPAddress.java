2177e488131c7b6e78d3b833677f89c70f000219
Md Hamid Azam <hm2563749@gmail.com>
2026-08-28 22:37:26 +0530

    Updating DefangingIPAddress

--- Strings/DefangingIPAddress.java
+++ Strings/DefangingIPAddress.java
@@ -2,10 +2,11 @@ package Strings;
 
 public class DefangingIPAddress {
     public static String DefangingIPAddress(String str) {
-        str = str.replace(".","[.]");
+        str = str.replace(".", "[.]");
 
         return str;
     }
+
     public static void main(String[] args) {
         String str = "1.1.1.1";
         System.out.println(DefangingIPAddress(str));
