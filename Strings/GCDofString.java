331203ab2e3485c0dfd2ee82c8104a4e120b3f1a
Md Hamid Azam <hm2563749@gmail.com>
2026-08-29 20:42:51 +0530

    Updating GCDofString

--- Strings/GCDofString.java
+++ Strings/GCDofString.java
@@ -2,21 +2,22 @@ package Strings;
 
 public class GCDofString {
     public static String GCD(String str1, String str2) {
-    //     int m = str1.length();
-    //     int n = str2.length();
+        // int m = str1.length();
+        // int n = str2.length();
 
-    //     if(!(str1 + str2).equals(str2 + str1)) return "";
+        // if(!(str1 + str2).equals(str2 + str1)) return "";
 
-    //     while(n != 0) {
-    //         int temp = n;
-    //         n = m%n;
-    //         m = temp;
-    //     }
+        // while(n != 0) {
+        // int temp = n;
+        // n = m%n;
+        // m = temp;
+        // }
 
-    //     return str1.substring(0, m);
-    // }
+        // return str1.substring(0, m);
+        // }
 
-    if (!(str1 + str2).equals(str2 + str1)) return "";
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
 
         int m = str1.length();
         int n = str2.length();
@@ -37,6 +38,7 @@ public class GCDofString {
 
         return str1.substring(0, gcd);
     }
+
     public static void main(String[] args) {
         String str1 = "ABCABC";
         String str2 = "ABC";
