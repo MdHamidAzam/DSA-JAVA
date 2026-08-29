253d8961435720335b8b83b33a54cd1b1df95324
Md Hamid Azam <hm2563749@gmail.com>
2026-08-29 20:45:09 +0530

    Updating GroupAnagrams

--- Strings/GroupAnagrams.java
+++ Strings/GroupAnagrams.java
@@ -1,18 +1,19 @@
 package Strings;
 
 import java.util.*;
+
 public class GroupAnagrams {
     public static List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
 
-        for(String str : strs) {
+        for (String str : strs) {
             char[] ch = str.toCharArray();
 
             Arrays.sort(ch);
 
             String key = new String(ch);
 
-            if(!map.containsKey(key)) {
+            if (!map.containsKey(key)) {
                 map.put(key, new ArrayList<>());
             }
             map.get(key).add(str);
@@ -20,8 +21,9 @@ public class GroupAnagrams {
 
         return new ArrayList<>(map.values());
     }
+
     public static void main(String[] args) {
-        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
         System.out.println(groupAnagrams(strs));
     }
 }
