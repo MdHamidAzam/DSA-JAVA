cd2b145d96f6ba9bfd14cdc8861e6045b4086cec
Md Hamid Azam <hm2563749@gmail.com>
2026-08-31 07:35:35 +0530

    Addding Determine if Two Strings Are Close

---
+++ Strings/StringsAreClose.java
@@ -0,0 +1,41 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class StringsAreClose {
+    public static boolean closeStrings(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        if(m != n) return false;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for(char ch : word1.toCharArray()) {
+            freq1[ch - 'a']++;
+        }
+
+        for(char ch : word2.toCharArray()) {
+            freq2[ch - 'a']++;
+        }
+
+        for(int i=0; i<26; i++) {
+            if((freq1[i] == 0) != (freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        return Arrays.equals(freq1, freq2);
+
+    }
+    public static void main(String[] args) {
+        String word1 = "cabbba";
+        String word2 = "abbccc";
+
+        System.out.println(closeStrings(word1, word2));
+    }
+}
