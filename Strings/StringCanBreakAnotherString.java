c0af679f85adfc6c284fb639e9056e63450bb3e9
Md Hamid Azam <hm2563749@gmail.com>
2026-09-01 23:08:37 +0530

    Updating StringCanBreakAnotherString

--- Strings/StringCanBreakAnotherString.java
+++ Strings/StringCanBreakAnotherString.java
@@ -3,7 +3,7 @@ package Strings;
 import java.util.Arrays;
 
 public class StringCanBreakAnotherString {
-    public static boolean checkIfCanBreak(String s1,  String s2) {
+    public static boolean checkIfCanBreak(String s1, String s2) {
         char[] a = s1.toCharArray();
         char[] b = s2.toCharArray();
 
@@ -13,13 +13,16 @@ public class StringCanBreakAnotherString {
         boolean s1BreakS2 = true;
         boolean s2BreakS1 = true;
 
-        for(int i=0; i<a.length; i++) {
-            if(a[i] < b[i]) s1BreakS2 = false;
-            if(b[i] < a[i]) s2BreakS1 = false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < b[i])
+                s1BreakS2 = false;
+            if (b[i] < a[i])
+                s2BreakS1 = false;
         }
 
         return s1BreakS2 || s2BreakS1;
     }
+
     public static void main(String[] args) {
         String s1 = "abc";
         String s2 = "xya";
