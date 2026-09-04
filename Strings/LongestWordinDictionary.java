f397e4a7357ab52cdc9f77440c55a55d6a711898
Md Hamid Azam <hm2563749@gmail.com>
2026-09-04 23:13:14 +0530

    Updating LongestWordinDictionary

--- Strings/LongestWordinDictionary.java
+++ Strings/LongestWordinDictionary.java
@@ -5,24 +5,25 @@ import java.util.List;
 
 public class LongestWordinDictionary {
     private static boolean isSubsequence(String s, String word) {
-        int i=0; 
-        int j=0;
+        int i = 0;
+        int j = 0;
 
-        while(i<s.length() && j<word.length()) {
-            if(s.charAt(i) == word.charAt(j)) j++;
+        while (i < s.length() && j < word.length()) {
+            if (s.charAt(i) == word.charAt(j)) j++;
             i++;
         }
 
         return j == word.length();
     }
+
     public static String findLongestWord(String s, List<String> dictionary) {
         String ans = "";
 
-        for(String word : dictionary) {
-            if(isSubsequence(s, word)) {
-                if(word.length() > ans.length() ||
-                   word.length() == ans.length() && 
-                   word.compareTo(ans) < 0) {
+        for (String word : dictionary) {
+            if (isSubsequence(s, word)) {
+                if (word.length() > ans.length() ||
+                        word.length() == ans.length() &&
+                                word.compareTo(ans) < 0) {
                     ans = word;
                 }
             }
@@ -30,9 +31,10 @@ public class LongestWordinDictionary {
 
         return ans;
     }
+
     public static void main(String[] args) {
         String s = "abpcplea";
-        List<String> dictionary = Arrays.asList("ale","apple","monkey","plea");
+        List<String> dictionary = Arrays.asList("ale", "apple", "monkey", "plea");
         System.out.println(findLongestWord(s, dictionary));
     }
 }
