f143392650fff9cfddad8dce12a2601725e873c0
Md Hamid Azam <hm2563749@gmail.com>
2026-09-07 08:33:28 +0530

    Adding ReverseLinkedListII

---
+++ LinkedList/ReverseLinkedListII.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+import LinkedList.DeleteNodeInLL.ListNode;
+
+public class ReverseLinkedListII {
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        for(int i=1; i<left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+        for(int i=0; i<right-left; i++) {
+            ListNode next = curr.next;
+
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+
+        return dummy.next;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        head = reverseBetween(head, 2, 5);
+        ListNode current = head;
+        
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+}
