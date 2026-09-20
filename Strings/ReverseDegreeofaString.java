0fe6b6353ceb9b6eb689852758190e512be70f91
Md Hamid Azam <hm2563749@gmail.com>
2026-09-20 21:34:48 +0530

    Adding ReverseDegreeofaString

---
+++ Strings/ReverseDegreeofaString.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class ReverseDegreeofaString {
+    public static int reverseDegree(String s) {
+        int n = s.length();
+
+        int ans = 0;
+
+        for(int i=0; i<n; i++) {
+            int revVal = 26 - (s.charAt(i) - 'a');
+            int position = i+1;
+
+            ans += revVal*position;
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        String s = "abc";
+        int result = reverseDegree(s);
+        System.out.println(result);
+    }
+}
