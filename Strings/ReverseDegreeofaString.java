fd47dbf9009533fa0fa2befaad79670518d45fa5
Md Hamid Azam <hm2563749@gmail.com>
2026-09-22 22:32:50 +0530

    Updating ReverseDegreeofaString

--- Strings/ReverseDegreeofaString.java
+++ Strings/ReverseDegreeofaString.java
@@ -5,15 +5,16 @@ public class ReverseDegreeofaString {
         int n = s.length();
         int ans = 0;
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             int revVal = 26 - (s.charAt(i) - 'a');
-            int position = i+1;
+            int position = i + 1;
 
-            ans += revVal*position;
+            ans += revVal * position;
         }
 
         return ans;
     }
+
     public static void main(String[] args) {
         String s = "abc";
         int result = reverseDegree(s);
